a2c993d2bd2aa003fe62b03e6febdc034de1a640
Raboro <marius.woerfelreal@gmail.com>
2023-02-21 23:05:35 +0100

    edited main

---
+++ src/main/java/org/tictactoeai/Main.java
@@ -0,0 +1,12 @@
+package org.tictactoeai;
+
+/**
+ * @author MariusWoerfel
+ * @since 1.0.0.SNAPSHOT
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
