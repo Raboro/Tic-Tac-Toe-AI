35159337a33b456bbfe8e8055d9463a5bcf7a1bd
Raboro <marius.woerfelreal@gmail.com>
2023-02-23 23:57:13 +0100

    initialized board fields

--- src/main/java/org/tictactoeai/Board.java
+++ src/main/java/org/tictactoeai/Board.java
@@ -12,6 +12,11 @@ public class Board {
 
     public Board() {
         fields = new Field[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                fields[i][j] = new Field();
+            }
+        }
     }
 
     public void setField(Sign sign, Position position) {
