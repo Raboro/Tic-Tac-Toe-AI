da576d23e6ecaf04e2b9ba6e8fdb018db3759110
Raboro <raborogit@gmail.com>
2023-02-25 16:25:40 +0100

    implemented move for user

--- src/main/java/org/tictactoeai/User.java
+++ src/main/java/org/tictactoeai/User.java
@@ -1,5 +1,8 @@
 package org.tictactoeai;
 
+import java.util.Optional;
+import java.util.Scanner;
+
 /**
  * Player class for the user, who plays against the AI
  *
@@ -14,6 +17,26 @@ public class User extends Player {
 
     @Override
     Position move() {
-        return Position.create(0, 0).orElse(null);
+        Optional<Position> position;
+        do {
+            position = getPosition();
+        } while (position.isEmpty());
+        return position.get();
+    }
+
+    private Optional<Position> getPosition() {
+        String[] input = getUserInput();
+        return isValidLength(input) ?
+                Position.create(Integer.parseInt(input[0]), Integer.parseInt(input[1])) :
+                Optional.empty();
+    }
+
+    private static String[] getUserInput() {
+        System.out.println("Select a valid Position [0-2] by type value for x and y separated with a space:");
+        return new Scanner(System.in).nextLine().split(" ");
+    }
+
+    private boolean isValidLength(String[] input) {
+        return input.length == 2;
     }
 }
